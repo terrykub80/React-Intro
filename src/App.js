1bd607f1b31ddb8104c3af73cca64b300bcaa18d
Terry Kubalewski <terrykub@yahoo.com>
2023-01-23 15:44:10 -0600

    move stat from out of each button to the app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
-function App() {
+function App(props) {
     let myName = 'Terry'
     let buttons = [
         {color:'primary', step: 1},
@@ -9,12 +10,19 @@ function App() {
         {color:'success', step: 100},
         {color:'danger', step: 1000},
     ]
+
+    const [count, setCount] = useState(0);    
+        
+    function handleClick(step){
+        setCount(count + step)
+    };
+
     return (
         <>
             <Navbar name={myName} city="Milton" test={123}/>
             <div className="container">
-                <h1>Hello {myName} {10*5} </h1>
-                {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx}/>)}
+                <h1>Hello {myName}, Count: {count}</h1>
+                {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx} handleClick={handleClick}/>)}
             </div>           
         </>
         );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react'
 
 export default function Button(props) {
-    const [count, setCount] = useState(0);    
-    
-    
-    function handleClick(){
-        console.log('Button clicked', count, props.step)
-        setCount(count + props.step)
-    }
-
+    console.log(props)
     return (
-        <button className={`btn btn-${props.color} w-100 mt-2`} onClick={handleClick}>+{props.step} - {count}</button>
+        <button className={`btn btn-${props.color} w-100 mt-2`} onClick={() => props.handleClick(props.step)}>+{props.step}</button>
     )
 }
