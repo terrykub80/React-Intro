c3d2453979e835977d652b2a47e4065f6589d697
Terry Kubalewski <terrykub@yahoo.com>
2023-01-23 15:13:17 -0600

    added stateful function components with useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 function App() {
     let myName = 'Terry'
+    let buttons = [
+        {color:'primary', step: 1},
+        {color:'secondary', step: 10},
+        {color:'success', step: 100},
+        {color:'danger', step: 1000},
+    ]
     return (
         <>
             <Navbar name={myName} city="Milton" test={123}/>
             <div className="container">
                 <h1>Hello {myName} {10*5} </h1>
-            </div>             
+                {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx}/>)}
+            </div>           
         </>
         );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default function Navbar(props) {
-    console.log(props);
-    console.log(typeof props);
+    // console.log(props);
+    // console.log(typeof props);
     return (
-        <nav className='navbar bg-body-tertiary'>
-            <div className='container-fluid'>
-                <a className='navbar-brand' href="/">Welcome {props.name} from {props.city}</a>
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Welcome {props.name} from {props.city}</a>
             </div>
         </nav>
   );
