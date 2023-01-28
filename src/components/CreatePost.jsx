eeb657ad4a90227dc9504d969e85faa6b89e020d
Terry Kubalewski <terrykub@yahoo.com>
2023-01-28 15:15:46 -0600

    handle Create Post on Submint

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -11,10 +11,42 @@ export default function CreatePost(props) {
         }
     })
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         console.log(event);
+
+        // Get the data from the form
+        let title = event.target.title.value;
+        let body = event.target.body.value;
+
+        // Get the token from localStorage
+        let token = localStorage.getItem('token');
+
+        // Set up the request headers
+        let myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('Authorization', `Bearer ${token}`);
+
+        // Set up the request body
+        let requestBody = JSON.stringify({title, body});
+
+        // Make the fetch request
+        let response = await fetch("http://localhost:5000/api/posts", {
+            method: 'POST',
+            headers: myHeaders,
+            body: requestBody
+        })
+
+        if (response.ok){
+            let data = await response.json();
+            props.flashMessage(`${data.title} has been created`, 'primary');
+            navigate('/');
+        } else {
+            props.flashMessage("There was an issue, please try again", 'warning')
+        };
+
     }
+
 
   return (
     <>
